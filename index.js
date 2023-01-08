8ade84957f597e280911f2ae185f69a5bf5b2f11
Les Carbonaro <lcarbonaro@gmail.com>
2023-01-07 23:17:49 -0500

    first commit

--- index.js
+++ index.js
@@ -5,25 +5,42 @@ http.createServer(async function (req, res) {
     //console.log(`Just got a request at ${req.url}!`)
     let {rows:data} = await getData();
 
+    console.log(`got ${data.length} rows`);
+
     let today = new Date();
     let numberOfDaysToAdd = 3;
-    let futureDate = today.setDate(today.getDate() + numberOfDaysToAdd);
+    let futureDate = today.setDate(today.getDate() + numberOfDaysToAdd);    
+    futureDate.setHours(23);
+    futureDate.setMinutes(59);
+    futureDate.setSeconds(59);
+    //futureDate = new Date(futureDate);
+
+    console.log(`future date cut-off is ${ new Date(futureDate) }`);
+
     let occDate;
 
     let msgData = data.filter(r => {
         occDate = new Date();
-        occDate.setMonth( parseInt(r.MthNum,10) );
+        occDate.setMonth( parseInt(r.MthNum,10) - 1);
         occDate.setDate(  parseInt(r.Day,10) );
+        occDate.setHours(0);
+        occDate.setMinutes(0);
+        occDate.setSeconds(0);
+        console.log(`occ date is ${ new Date(occDate) }`);
         return occDate <= futureDate;
     });
 
     if( msgData.length ) {
 
+        console.log(`have ${msgData.length} upcoming bdays etc`);
+
         let emailData = msgData.map(r => {
-            return `<li>${r.Month}&nbsp;${r.Day} - ${r.Occasion} of ${r.Name} (${r.Relation})</li>`;
+            return `<li>${r.Month} ${r.Day} - ${r.Occasion} of ${r.Name} (${r.Relation})</li>`;
         });
 
         let emailHtml = `<ul>${emailData.join(' ')}</ul>`;
+
+        console.log(emailHtml);
 
         const transporter = nodemailer.createTransport({
             service: 'gmail',
@@ -36,8 +53,8 @@ http.createServer(async function (req, res) {
         const mailOptions = {
             from: process.env.FROM_EMAIL,
             to: process.env.TO_EMAIL,
-            subject: 'WHATEVER Subject test2',
-            html: 'SOME whatever Email content test2'
+            subject: 'Upcoming Birthdays Etc.',
+            html: emailHtml
         };
     
         transporter.sendMail(mailOptions, function (error, info) {
